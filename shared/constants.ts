42810fdde5c6fc102c381450d634d4b4576b88e6
Nahom Assefa <nzenebe@gmail.com>
2025-08-25 08:37:44 -0500

    prod testing

--- shared/constants.ts
+++ shared/constants.ts
@@ -7,8 +7,8 @@
 
 export const COMPANY_INFO = {
   // Contact Information
-  PHONE: "(651) 500-6198",
-  PHONE_CLEAN: "+16515006198",
+  PHONE: "(612) 597-4248",
+  PHONE_CLEAN: "+16125974248",
   EMAIL: "hollytransport04@gmail.com",
   
   // Company Details
